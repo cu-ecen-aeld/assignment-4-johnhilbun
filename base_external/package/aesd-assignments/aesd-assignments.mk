393678ed8d1656afabd80ed7f0cd46ae82108b91
johnhilbun <john.hilbun@gmail.com>
2025-12-06 05:04:57 +0000

    This version of Assignment 3 and later passed Assignment 4 Part 2 local testing

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,18 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+# JDH original : AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+# JDH full hash : AESD_ASSIGNMENTS_VERSION = 'f589d6f4fda37a4a5095e13dec52a62d4720f8f5'
+# JDH AESD_ASSIGNMENTS_VERSION = 'f589d6f'
+# JDH works : AESD_ASSIGNMENTS_VERSION = assignment-3-part-2
+# JDH found this : AESD_ASSIGNMENTS_VERSION = f589d6f
+# JDH QEMU worked but outdate : AESD_ASSIGNMENTS_VERSION = e94de1d 
+AESD_ASSIGNMENTS_VERSION = ecac491
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+# JDH original : AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-johnhilbun.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -23,6 +30,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	# JDH added
+	$(INSTALL) -m 0755   $(@D)/finder-app/writer  $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755   $(@D)/finder-app/finder.sh  $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755   $(@D)/finder-app/finder-test.sh  $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
